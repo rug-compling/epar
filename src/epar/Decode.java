4ae366e961ee25d87a58a6028ffdedee893ac2af
Kilian Evang <noreply@texttheater.net>
2015-08-12 20:51:38 +0100

    Parallel decoding

--- src/epar/Decode.java
+++ src/epar/Decode.java
@@ -9,7 +9,11 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
 
 import epar.data.Sentence;
 import epar.grammar.Grammar;
@@ -23,13 +27,11 @@ import epar.util.StringUtil;
 
 public class Decode {
 
-	private final static Logger LOGGER = Logger.getLogger(Decode.class
-			.getName());
+	//private final static Logger LOGGER = Logger.getLogger(Decode.class.getName());
 
-	public static Agenda decode(Agenda agenda, Grammar grammar, Model model,
-			Oracle oracle) {
-		//LOGGER.info("Input agenda: " + agenda.getCandidates());
-		
+	public static Agenda decode(Agenda agenda, Grammar grammar, Model model, Oracle oracle) {
+		// LOGGER.info("Input agenda: " + agenda.getCandidates());
+
 		Agenda nextAgenda = agenda.nextAgenda(grammar, model, oracle);
 
 		if (nextAgenda.noneCorrect()) {
@@ -43,18 +45,16 @@ public class Decode {
 		return decode(nextAgenda, grammar, model, oracle);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		if (args.length < 5) {
-			System.err.println("USAGE: java Decode SENTENCES.IN RULES.BIN "
-					+ "RULES.UN TREES.OUT MODEL1 MODEL2 ... MODELN");
+			System.err.println(
+					"USAGE: java Decode SENTENCES.IN RULES.BIN " + "RULES.UN TREES.OUT MODEL1 MODEL2 ... MODELN");
 			System.exit(1);
 		}
 
 		try {
-			List<Sentence> inputSentences = Sentence.readSentences(new File(
-					args[0]));
-			Grammar grammar = Grammar
-					.load(new File(args[1]), new File(args[2]));
+			List<Sentence> inputSentences = Sentence.readSentences(new File(args[0]));
+			final Grammar grammar = Grammar.load(new File(args[1]), new File(args[2]));
 			File outputFile = new File(args[3]);
 			List<File> modelFiles = new ArrayList<File>(args.length - 4);
 
@@ -62,23 +62,36 @@ public class Decode {
 				modelFiles.add(new File(args[i]));
 			}
 
-			Model model = Model.load(modelFiles);
-			Oracle oracle = new AcceptAllOracle();
+			final Model model = Model.load(modelFiles);
+			final Oracle oracle = new AcceptAllOracle();
+			
+			List<Future<String>> parses = new ArrayList<Future<String>>(inputSentences.size());
+			ForkJoinPool pool = new ForkJoinPool();
+			
+			// Schedule sentences for parsing
+			for (final Sentence sentence : inputSentences) {
+				@SuppressWarnings("serial")
+				ForkJoinTask<String> parse = new RecursiveTask<String>() {
 
-			try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream(outputFile), "utf-8"))) {
-				// TODO concurrency
-				int i = 1;
-				
-				for (Sentence sentence : inputSentences) {
-					LOGGER.info("Decoding sentence " + i++);
+					@Override
+					protected String compute() {
+						Agenda result = decode(Agenda.initial(sentence), grammar, model, oracle);
+						List<Node> nodes = ListUtil.listFromIterable(result.getHighestScoring().item.stack);
+						Collections.reverse(nodes);
+						return StringUtil.join(nodes, " ") + "\n";
+					}
 					
-					Agenda result = decode(Agenda.initial(sentence), grammar,
-							model, oracle);
-					List<Node> nodes = ListUtil.listFromIterable(result
-							.getHighestScoring().item.stack);
-					Collections.reverse(nodes);
-					writer.write(StringUtil.join(nodes, " ") + "\n");
+				};
+				
+				pool.execute(parse);
+				parses.add(parse);
+			}
+			
+			// Retrieve parses and write them to STDOUT
+			try (Writer writer = new BufferedWriter(
+					new OutputStreamWriter(new FileOutputStream(outputFile), "utf-8"))) {
+				for (Future<String> parse : parses) {
+					writer.write(parse.get());
 				}
 			}
 		} catch (IOException e) {
