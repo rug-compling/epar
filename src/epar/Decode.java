6be55d01c43871a1550e06536e4e53c05f7c082b
Kilian Evang <noreply@texttheater.net>
2015-08-13 13:00:13 +0100

    Log agendas again for debugging

--- src/epar/Decode.java
+++ src/epar/Decode.java
@@ -31,7 +31,7 @@ public class Decode {
 	private final static Logger LOGGER = Logger.getLogger(Decode.class.getName());
 
 	public static Agenda decode(Agenda agenda, Grammar grammar, Model model, Oracle oracle) {
-		// LOGGER.info("Input agenda: " + agenda.getCandidates());
+		LOGGER.info("Input agenda: " + agenda.getCandidates());
 
 		Agenda nextAgenda = agenda.nextAgenda(grammar, model, oracle);
 
