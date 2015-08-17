7cf34c1e156b2e9b8b50c3c690804641b3c86f98
Kilian Evang <noreply@texttheater.net>
2015-08-17 12:23:45 +0100

    Added hack to write all features seen to a file

--- src/epar/model/AveragingModel.java
+++ src/epar/model/AveragingModel.java
@@ -9,8 +9,10 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import epar.parser.Action;
 import epar.parser.Candidate;
@@ -18,11 +20,15 @@ import epar.parser.Candidate;
 public class AveragingModel implements Model {
 
 	private final Map<String, Map<Action, AveragingWeight>> weights = new HashMap<String, Map<Action, AveragingWeight>>();
+	
+	public final Set<String> seenFeatures = new HashSet<String>();
 
 	public double score(List<String> stateFeatures, Action action) {
 		double score = 0;
 
 		for (String feature : stateFeatures) {
+			seenFeatures.add(feature + " " + action);
+			
 			// Either: avoid creating weights that are never updated.
 			if (!weights.containsKey(feature) || !weights.get(feature).containsKey(action)) {
 				continue;
@@ -88,6 +94,15 @@ public class AveragingModel implements Model {
 		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"))) {
 			for (String line : toLines(currentStateCount)) {
 				writer.write(line);
+			}
+		}
+	}
+
+	public void saveSeenFeatures(File file) throws IOException {
+		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"))) {
+			for (String feature : seenFeatures) {
+				writer.write(feature);
+				writer.write("\n");
 			}
 		}
 	}
