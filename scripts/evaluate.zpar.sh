249195e0c46c4e3cd48cac6119e16329807ea8c6
Kilian Evang <noreply@texttheater.net>
2015-08-19 20:06:56 +0200

    Evaluation almost (?) there.
    Remaining problem: our grammar is too big and contains rules that generate
    can't handle.

--- scripts/evaluate.zpar.sh
+++ scripts/evaluate.zpar.sh
@@ -2,7 +2,8 @@
 
 # Adapted from ZPar repository
 
-#set -e
+set -e
+#set -x
 
 scripts=ext/zpar/scripts
 candc=ext/candc
@@ -31,22 +32,15 @@ do
     echo parser failed.
     echo ''>>$1.ccgbank_deps
   else
-    # FIXME for fragmentary parses, this throws away all but the first fragment.
-    # What format do we need to use to prevent this?
-    python $scripts/lang/chinese/ctbtools/unbinarize.py -op $scratch/$1.txt.part.$i >$scratch/$1.pipe.part.$i
-    >&2 echo "unbinarized" 
+    python scripts/zpar2pipe.py -op $scratch/$1.txt.part.$i >$scratch/$1.pipe.part.$i
     python $scripts/ccg/pipe.py split $scratch/$1.pipe.part.$i $scratch/$1.cat.part.$i $scratch/$1.pipe.fragmented.part.$i
-    >&2 echo "piped" 
     $candc/bin/generate -j $candc/src/data/ccg/cats $candc/src/data/ccg/cats/markedup $scratch/$1.pipe.fragmented.part.$i >$scratch/$1.ccgbank_deps.part.$i
-    >&2 echo "generated" 
-    python $scripts/ccg/mergefragmenteddeps.py $scratch/$1.ccgbank_deps.part.$i $scratch/$1.pipe.fragmented.part.$i $scratch/$1.cat.part.$i >$scratch/$1.ccgbank_deps.formatted.part.$i
-    >&2 echo "merged" 
+    python scripts/mergefragmenteddeps.py $scratch/$1.ccgbank_deps.part.$i $scratch/$1.pipe.fragmented.part.$i $scratch/$1.cat.part.$i >$scratch/$1.ccgbank_deps.formatted.part.$i
     if [ `$candc/bin/generate -T $candc/src/data/ccg/cats/ $candc/src/data/ccg/cats/markedup $scratch/$1.pipe.fragmented.part.$i | grep '__PARSE_FAILED__' | wc -l | cut -d\  -f1` == 0 ]
 #    if [ `wc -w $scratch/$1.ccgbank_deps.formatted.part.$i|cut -d\  -f1` == 0 ] 
     then
-      grep ^[^\#] $scratch/$1.ccgbank_deps.formatted.part.$i >>$1.ccgbank_deps
+      grep ^[^\#] $scratch/$1.ccgbank_deps.formatted.part.$i | cat >>$1.ccgbank_deps
       cat $scratch/$1.cat.part.$i >>$1.ccgbank_deps
-      >&2 echo "appended"
     else 
       echo bin/generator failed
       echo '' >>$1.ccgbank_deps
