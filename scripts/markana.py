e223c955458e92ba2b58e64100361eb8cc13e1a2
Kilian Evang <noreply@texttheater.net>
2015-09-14 20:18:48 +0200

    Added script to analyze markedup file

---
+++ scripts/markana.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import collections
+import sys
+import util
+
+mapping = collections.defaultdict(set)
+mapping_straight = collections.defaultdict(set)
+
+with open('ext/candc/src/data/ccg/cats/markedup') as f:
+    for block in util.blocks(
+            l for l in f if not (l.startswith('=') or l.startswith('#'))):
+        if not block:
+            continue
+        block = block.split('\n')
+        cat = block[0].rstrip()
+        markedup = block[1].split()[1]
+        cat_straight = cat.replace('\\', '/')
+        markedup_straight = markedup.replace('\\', '/')
+        mapping[cat].add(markedup)
+        mapping_straight[cat_straight].add(markedup_straight)
+
+print('Dependency-ambiguous original categories:')
+
+for cat, markedups in mapping.items():
+    if len(markedups) > 1:
+        print(cat)
+
+print('Dependency-ambiguous straight categories:')
+
+for cat, markedups in mapping_straight.items():
+    if len(markedups) > 1:
+        print(cat)
