9bf51c37f89ee9f7825caf5aae98908a008f3587
Kilian Evang <noreply@texttheater.net>
2015-09-30 09:12:51 +0200

    Extended script to also eat stagged lines

--- scripts/straighten_ccgbank_deps.py
+++ scripts/straighten_ccgbank_deps.py
@@ -8,8 +8,11 @@ def is_empty(line):
 def is_comment(line):
     return line.startswith('#')
 
+def is_stagged_line(line):
+    return line.startswith('<c> ')
+
 for line in sys.stdin:
-    if not (is_empty(line) or is_comment(line)):
+    if not (is_empty(line) or is_comment(line) or is_stagged_line(line)):
         before, cat, after = line.split(' ', 2)
         cat = cat.replace('\\', '/')
         line = ' '.join((before, cat, after))
