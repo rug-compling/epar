73b88054f0fadeaaade18538fa1fcd019107066c
Kilian Evang <noreply@texttheater.net>
2015-10-06 17:13:04 +0200

    Code formatting

--- src/epar/util/NEStack.java
+++ src/epar/util/NEStack.java
@@ -4,59 +4,59 @@ import java.util.Iterator;
 
 public class NEStack<T> extends Stack<T> {
 
-	private T first;
-	private Stack<T> rest;
+    private final T first;
+    private final Stack<T> rest;
 
-	public NEStack(T first, Stack<T> rest) {
-		this.first = first;
-		this.rest = rest;
-	}
+    public NEStack(T first, Stack<T> rest) {
+        this.first = first;
+        this.rest = rest;
+    }
 
-	@Override
-	public boolean isEmpty() {
-		return false;
-	}
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 
-	@Override
-	public T getFirst() {
-		return first;
-	}
+    @Override
+    public T getFirst() {
+        return first;
+    }
 
-	@Override
-	public Stack<T> getRest() {
-		return rest;
-	}
+    @Override
+    public Stack<T> getRest() {
+        return rest;
+    }
 
-	@Override
-	public Iterator<T> iterator() {
-		final Stack<T> stack = this;
+    @Override
+    public Iterator<T> iterator() {
+        final Stack<T> stack = this;
 
-		return new Iterator<T>() {
+        return new Iterator<T>() {
 
-			Stack<T> next = stack;
+            Stack<T> next = stack;
 
-			@Override
-			public boolean hasNext() {
-				return !next.isEmpty();
-			}
+            @Override
+            public boolean hasNext() {
+                return !next.isEmpty();
+            }
 
-			@Override
-			public T next() {
-				T result = next.getFirst();
-				next = next.getRest();
-				return result;
-			}
+            @Override
+            public T next() {
+                T result = next.getFirst();
+                next = next.getRest();
+                return result;
+            }
 
-			@Override
-			public void remove() {
-				throw new UnsupportedOperationException();
-			}
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
 
-		};
-	}
+        };
+    }
 
-	@Override
-	public int size() {
-		return 1 + rest.size();
-	}
+    @Override
+    public int size() {
+        return 1 + rest.size();
+    }
 }
--- src/epar/util/Stack.java
+++ src/epar/util/Stack.java
@@ -4,28 +4,28 @@ import java.util.NoSuchElementException;
 
 public abstract class Stack<T> implements Iterable<T> {
 
-	public Stack<T> push(T element) {
-		return new NEStack<T>(element, this);
-	}
-	
-	public abstract boolean isEmpty();
-	
-	public abstract T getFirst() throws NoSuchElementException;
-	
-	public abstract Stack<T> getRest() throws NoSuchElementException;
-	
-	public T get(int index, T defaultValue) {
-		if (isEmpty()) {
-			return defaultValue;
-		}
-		
-		if (index == 0) {
-			return getFirst();
-		}
-		
-		return getRest().get(index - 1, defaultValue);
-	}
+    public Stack<T> push(T element) {
+        return new NEStack<>(element, this);
+    }
 
-	public abstract int size();
+    public abstract boolean isEmpty();
+
+    public abstract T getFirst() throws NoSuchElementException;
+
+    public abstract Stack<T> getRest() throws NoSuchElementException;
+
+    public T get(int index, T defaultValue) {
+        if (isEmpty()) {
+            return defaultValue;
+        }
+
+        if (index == 0) {
+            return getFirst();
+        }
+
+        return getRest().get(index - 1, defaultValue);
+    }
+
+    public abstract int size();
 
 }
