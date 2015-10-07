50c5697cef126bc2688d18e557b6ec981a3e3d24
Kilian Evang <noreply@texttheater.net>
2015-10-07 14:55:01 +0200

    Action: added fromString and sequencefromString

--- src/epar/parser/Action.java
+++ src/epar/parser/Action.java
@@ -2,28 +2,80 @@ package epar.parser;
 
 import epar.util.SymbolPool;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Action {
-    
+
     private static final short TYPE_INIT = 1;
-    
+
     private static final short TYPE_SHIFT = 2;
-    
+
     private static final short TYPE_BINARY = 3;
-    
+
     private static final short TYPE_UNARY = 4;
-    
+
     private static final short TYPE_FINISH = 5;
-    
+
     private static final short TYPE_IDLE = 6;
-    
+
     private static final short TYPE_SKIP = 7;
-    
+
+    public static List<Action> sequenceFromString(String sequenceString) {
+        String[] actionStrings = sequenceString.split(" ");
+        List<Action> actionSequence = new ArrayList<>(actionStrings.length);
+
+        for (String actionString : actionStrings) {
+            actionSequence.add(Action.fromString(actionString));
+        }
+
+        return actionSequence;
+    }
+
+    public static Action fromString(String actionString) {
+        String[] parts = actionString.split("-");
+
+        switch (parts[0]) {
+            case "INIT":
+                checkArgs(actionString, parts, 0);
+                return INIT;
+            case "SHIFT":
+                checkArgs(actionString, parts, 1); // TODO add semantics
+                return shift(SymbolPool.getID(parts[1]));
+            case "BINARY":
+                checkArgs(actionString, parts, 1);
+                return binary(SymbolPool.getID(parts[1]));
+            case "UNARY":
+                checkArgs(actionString, parts, 1);
+                return unary(SymbolPool.getID(parts[1]));
+            case "FINISH":
+                checkArgs(actionString, parts, 0);
+                return FINISH;
+            case "IDLE":
+                checkArgs(actionString, parts, 0);
+                return IDLE;
+            case "SKIP":
+                checkArgs(actionString, parts, 0);
+                return SKIP;
+            default:
+                throw new IllegalArgumentException("Invalid action type: " + parts[0]);
+        }
+    }
+
+    private static void checkArgs(String actionString, String[] parts, int expectedNumArgs) {
+        int actualNumArgs = parts.length - 1;
+
+        if (actualNumArgs != expectedNumArgs) {
+            throw new IllegalArgumentException("Invalid action " + actionString + ": expected " + expectedNumArgs + " arguments for " + parts[0] + ", got " + actualNumArgs);
+        }
+    }
+
     /**
      * The action type: 1 for init, 2 for shift, 3 for binary, 4 for unary, 5
      * for finish and 6 for idle, 7 for skip.
      */
     public final short type;
-    
+
     /**
      * The category, or {@link SymbolPool.NONE} for action types that are not
      * associated with a category.
